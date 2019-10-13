f17bcc0267105fe8858aa1d16035e1764a25ee36
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2019-10-13 03:58:04 +0200

    chore: removed uneccesary flask app context from tests

--- api/tests/test_database.py
+++ api/tests/test_database.py
@@ -12,12 +12,10 @@ def preapre(request):
     mongo.db.drop_collection('users')
 
 def test_that_query_user_is_none():
-    with app.app_context():
-        user = get_user('123')
-        assert user == None
+    user = get_user('123')
+    assert user == None
 
 def test_that_query_user_is_not_none_after_inserting_a_given_user():
-    with app.app_context():
-        insert_user('123')
-        user = get_user('123')
-        assert user != None
+    insert_user('123')
+    user = get_user('123')
+    assert user != None
