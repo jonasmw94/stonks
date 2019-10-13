3461e2646f8a148d55d1b9c15730a74c99650e23
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2019-10-13 03:01:12 +0200

    chore: Moved all token logic to its own folder

--- api/main/__init__.py
+++ api/main/__init__.py
@@ -1,30 +1,11 @@
 from flask import Flask, jsonify, request, make_response
-import jwt
 import datetime
 import markdown
 import os
-from functools import wraps
+from api.token import generate_token, decode_token, token_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'VERYSECRET!!!!!!!!'
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.args.get('token')
-
-        if not token:
-            return jsonify({ 'message': 'Token is missing' }), 403
-        
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-        except:
-            return jsonify({ 'message': 'Token is invalid' }), 403
-
-        return f(*args, **kwargs)
-
-    return decorated
-
 
 @app.route("/")
 def index():
@@ -43,6 +24,5 @@ def login():
     auth = request.authorization
 
     if auth and auth.password == 'password':
-        token = jwt.encode({ 'user': auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30) }, app.config['SECRET_KEY'])
-        return jsonify({'token' : token.decode('UTF-8')})
+        return generate_token(auth.username, app.config['SECRET_KEY'])
     return make_response('Could not verify!', 401, { 'WWW-Authenticate' : 'Basic realm="Login required"' })
