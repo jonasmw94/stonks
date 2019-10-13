d935ff34153d706e59f6411c09c4b414e49380e4
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2019-10-13 17:48:29 +0200

    chore: Fixed user creation logic

--- api/database/__init__.py
+++ api/database/__init__.py
@@ -7,7 +7,7 @@ mongo = PyMongo(app)
 def validate_username(username: str) -> bool:
     user = get_user(username)
 
-    if user is not None:
+    if user != None:
         return False
     
     if len(username) < 3:
@@ -16,19 +16,23 @@ def validate_username(username: str) -> bool:
     return True
 
 def validate_password(password: str) -> bool:
-    if len(password < 8):
+    if len(password) < 8:
         return False
+
+    return True
 
 def get_user(username: str) -> str:
     collection = mongo.db['users']
     user = collection.find_one({ 'user': username })
     return user
 
-def insert_user(username: str, password: str) -> None:
+def insert_user(username: str, password: str) -> bool:
     if not validate_password(password) or not validate_username(username):
-        return
+        return False
     
     salt = gensalt()
-    hashedpw = hashpw(password, salt)
+    hashedpw = hashpw(password.encode("utf-8"), salt)
     collection = mongo.db['users']
     collection.insert_one({ 'user': username, 'salt': salt, 'hashedpw': hashedpw })
+
+    return True
